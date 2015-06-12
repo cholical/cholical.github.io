76247eafdaa987ac0c5b09bb65316017b4c43ee5
cholical <ronding@live.unc.edu>
2015-06-12 14:29:20 -0400

    Updated textbooks

--- app/textbooks/textbooks.controller.js
+++ app/textbooks/textbooks.controller.js
@@ -1,10 +1,16 @@
 (function () {
 
   'use strict';
-  var app;
+  var app, onTextbookSuccess;
 
   app = angular.module("tclassified");
   app.controller('textbooksCtrl', ['$scope', 'textbooksSvc', '$stateParams', '$log', function textbooksCtrl($scope, textbooksSvc, $stateParams, $log){
+
+    $scope.textbooks = textbooksSvc.getTextbooks();
+    $scope.pageSize = 10;
+    $scope.currentPage = 1;
+    $scope.sortType = "textbookName";
+    $scope.sortReverse = false;
     
   }]);
 }());
--- app/textbooks/textbooks.service.js
+++ app/textbooks/textbooks.service.js
@@ -1,18 +1,75 @@
 (function () {
 
   "use strict";
-  var app, getTextbooks;
+  var app, makeTextbooks, getTextbooks, postTextbooks;
   
   app = angular.module('tclassified');
 
   app.factory('textbooksSvc', ['$http', '$log', function textbooksSvc($http, $log) {
+
+
     
-    getTextbooks = function () {
-      return "Hello";
+    makeTextbooks = function () {
+      //Sample textbooks stored on the front end
+      return [
+        {
+          textbookName: "Ronald's Book",
+          author: "Ronald Ding",
+          edition: 12,
+          classes: [
+            "Bladder Controll 101", "AMST 101"
+          ],
+          price: 100,
+          acceptingOffers: true,
+          sellerName: "Cholical",
+          description: "Buy this book please",
+          contactInfo: "abc123@yahoo.com",
+          password: "fuck"
+        },
+        {
+          textbookName: "Mac's Book",
+          author: "Mac",
+          edition: 3,
+          classes: [
+            "Not a real class 123", "BUTT 101"
+          ],
+          price: 10,
+          acceptingOffers: true,
+          sellerName: "mac",
+          description: "cheap",
+          contactInfo: "macattack@gmail.com",
+          password: "fuck"
+        },
+        {
+          textbookName: "One more dumb book",
+          author: "nobody cares",
+          edition: 101,
+          classes: [
+            "bobsledding 201", "NIPS 101"
+          ],
+          price: 2,
+          acceptingOffers: false,
+          sellerName: "Cholical",
+          description: "urination attacks",
+          contactInfo: "abc123@yahoo.com",
+          password: "fuck"
+        },
+      ]
     };
 
+    getTextbooks = function() {
+      //This function will eventually be the ajax call to the backend server
+      return makeTextbooks();
+    }
+
+    postTextbooks = function() {
+      //This function will eventually be the ajax push to the backend server
+    }
+
     return {
+      makeTextbooks: makeTextbooks,
       getTextbooks: getTextbooks,
+      postTextbooks: postTextbooks
      
     };
   }]);
