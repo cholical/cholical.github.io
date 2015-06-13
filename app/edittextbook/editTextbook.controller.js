dc7348c138b6a304df58ecdf4477bcf462c3a2f4
cholical <ronding@live.unc.edu>
2015-06-13 17:27:48 -0400

    Edit textbooks feature with password now working

---
+++ app/edittextbook/editTextbook.controller.js
@@ -0,0 +1,57 @@
+(function () {
+
+  'use strict';
+  var app;
+
+  app = angular.module("tclassified");
+  app.controller('editTextbookCtrl', ['$scope', '$modalInstance', 'newListingSvc', '$stateParams', '$log', 'data', function editTextbookCtrl($scope, $modalInstance, newListingSvc, $stateParams, $log, data){
+
+    $scope.storedTextbook = data;
+    $scope.newListing = angular.copy($scope.storedTextbook);
+    $scope.class2 = true;
+    $scope.class3 = true;
+    $scope.class4 = true;
+    $scope.class5 = true;
+    
+    $scope.addClass2 = function() {
+        $scope.class2 = false;
+    };
+    $scope.addClass3 = function() {
+        $scope.class3 = false;
+    };
+    $scope.addClass4 = function() {
+        $scope.class4 = false;
+    };
+    $scope.addClass5 = function() {
+        $scope.class5 = false;
+    };
+
+    $scope.displayClasses = function() {
+        var numClasses = $storedTextbook.classes.length
+    }
+
+    $scope.close = function() {
+        $modalInstance.dismiss('close');
+    };
+    $scope.submitListing = function() {
+        $scope.storedTextbook.textbookName = $scope.newListing.textbookName;
+        $scope.storedTextbook.author = $scope.newListing.author;
+        $scope.storedTextbook.edition = $scope.newListing.edition;
+        $scope.storedTextbook.classes = angular.copy($scope.newListing.classes);
+        $scope.storedTextbook.price = $scope.newListing.price;
+        $scope.storedTextbook.acceptingOffers = $scope.newListing.acceptingOffers;
+        $scope.storedTextbook.sellerName = $scope.newListing.sellerName;
+        $scope.storedTextbook.description = $scope.newListing.description;
+        $scope.storedTextbook.contactInfo = $scope.newListing.contactInfo;
+        $scope.storedTextbook.password == $scope.newListing.password;
+          
+        $modalInstance.close();
+    };
+
+
+
+    
+  }]);
+}());
+
+
--- app/textbookinfo/textbookInfo.controller.js
+++ app/textbookinfo/textbookInfo.controller.js
@@ -4,16 +4,32 @@
   var app;
 
   app = angular.module("tclassified");
-  app.controller('textbookInfoCtrl', ['$scope', '$modalInstance', '$stateParams', '$log', 'data', function textbookInfoCtrl($scope, $modalInstance, $stateParams, $log, data){
+  app.controller('textbookInfoCtrl', ['$scope', '$modalInstance', '$modal', '$stateParams', '$log', 'data', function textbookInfoCtrl($scope, $modalInstance, $modal, $stateParams, $log, data){
 
     $scope.textbook = data;
     $scope.close = function () {
 		    $modalInstance.dismiss('close');
-	};
-	$scope.passwordInput;
+	  };
+	  $scope.passwordInput;
+    $scope.checkPasswordInput = function(currentTextbook) {
+        console.log($scope.passwordInput)
+        if ($scope.passwordInput === $scope.textbook.password) {
+            var modalInstance = $modal.open({
+            templateUrl: 'app/edittextbook/editTextbook.html',
+            controller: 'editTextbookCtrl',
+            resolve: {
+                data: function() {
+                    return currentTextbook;
+                }
+            },
+            size: 'lg'
+            })
+        }
+        else {
+          console.log("password incorrect");
+        }
+    }
 
-
-    
   }]);
 }());
 
