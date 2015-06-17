cf9d095e30d1ada8c8779587b0f42e57cc3cf070
macvogelsang <macvogelsang@gmail.com>
2015-06-16 21:12:37 -0400

    some about page styling WIP

--- app/about/about.controller.js
+++ app/about/about.controller.js
@@ -11,4 +11,27 @@
     }
     
   }]);
+
+  
+$('.profile').on('mouseenter',function(){
+    $scope.$apply(function(){
+        var riseHeight = $(this).find('.profileInfo').css('height') - 42;
+        //dont forget replace magic numbers later mac
+        var newTop = 
+        $(this).find('.profileInfo').animate({
+            top:'284px',
+        },400);
+    });
+    
+
+}).on('mouseleave',function(){
+    $scope.$apply(function(){
+        $(this).find('.profileInfo').animate({
+            top:'360px',
+        },400);
+    });
+});
+
+
+  
 }());
--- styleScript.js
+++ styleScript.js
@@ -4,4 +4,21 @@ $(document).ready(function(){
         $('.clickSort').removeClass('activeSort');
         $(this).addClass('activeSort');
     });
+
+    // $(".profile img").each(function () {
+    //     //get height and width (unitless) and divide by 2
+    //     var hWide = ($(this).width()) / 2; //half the image's width
+    //     var hTall = ($(this).height()) / 2; //half the image's height, etc.
+
+    //     // attach negative and pixel for CSS rule
+    //     hWide = '-' + hWide + 'px';
+    //     hTall = '-' + hTall + 'px';
+
+    //     $(this).addClass("js-fix").css({
+    //         "margin-left": hWide,
+    //             "margin-top": hTall
+    //     });
+    // });
+    
+    
 });
