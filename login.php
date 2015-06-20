66f0a66bcb64776da36b958a41edfb02a8618725
cholical <ronding@live.unc.edu>
2015-06-19 21:32:58 -0400

    added remote database

--- login.php
+++ login.php
@@ -1,6 +1,27 @@
 <?php // login.php 
-$db_hostname = 'localhost:3306'; //url of mysql database location
+
+//db4Free Login
+$db_hostname = 'db4free.net'; //url of mysql database location
 $db_database = 'tclassified'; 
-$db_username = 'user'; 
-$db_password = 'cholical'; 
+$db_username = 'cholical'; 
+$db_password = 'h52FGdek!'; 
+
+
+//000Hosting Login
+// $db_hostname = 'mysql3.000webhost.com/a7412793_tclassi'; //url of mysql database location
+// $db_database = 'a7412793_tclassi'; 
+// $db_username = 'a7412793_tclassi'; 
+// $db_password = 'abc123'; 
+
+
+
+
+//Localhost Login
+// $db_hostname = 'localhost:3306'; //url of mysql database location
+// $db_database = 'tclassified'; 
+// $db_username = 'user'; 
+// $db_password = 'cholical'; 
+
+
 ?>
+
